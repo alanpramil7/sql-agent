40e377a0110f79e30d5db4f66a152473a0061c83
Alan <alanpramil7@gmail.com>
2025-02-21 22:12:52 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain_groq import ChatGroq
+from langchain import hub
+from langgraph.prebuilt import create_react_agent
+
+load_dotenv()
+
+db = SQLDatabase.from_uri("postgresql://al:12345@localhost:5432/postgres")
+# llm = ChatGroq(model="deepseek-r1-distill-llama-70b", temperature=0)
+llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
+
+toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+tools = toolkit.get_tools()
+
+
+prompt_template = hub.pull("langchain-ai/sql-agent-system-prompt")
+
+system_message = prompt_template.format(dialect="postgresql", top_k=5)
+print(prompt_template.messages[0].pretty_print())
+
+agent_executer = create_react_agent(llm, tools, prompt=system_message)
+
+question = "Which product has most amount of stock?"
+
+for step in agent_executer.stream(
+    {"messages": [{"role": "user", "content": question}]},
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
